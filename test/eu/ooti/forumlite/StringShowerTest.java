bba3a359429e9719cf267d907d5e02e9ae92c5bc
unknown <mpetkovic@nbwin1168.campus.tue.nl>
2015-10-07 17:23:25 +0200

    Add StringShowerTest class

---
+++ test/eu/ooti/forumlite/StringShowerTest.java
@@ -0,0 +1,61 @@
+package eu.ooti.forumlite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringShowerTest {
+
+	@Test
+	public void testConvertChar() throws Exception {
+		StringShower ss = new StringShower();
+		assertArrayEquals(ss.ConvertChar('1'), new int [][]
+			{{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0}});
+		
+		assertArrayEquals(ss.ConvertChar('O'), new int [][]
+			{{1, 0, 1, 0, 1},
+			{0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 1},
+			{0, 0, 0, 0, 0},
+			{1, 0, 1, 0, 1}});
+		
+		assertArrayEquals(ss.ConvertChar('o'), new int [][]
+			{{0, 0, 0, 0, 0},
+			{0, 1, 1, 1, 0},
+			{0, 1, 0, 1, 0},
+			{0, 1, 1, 1, 0},
+			{0, 0, 0, 0, 0}});
+		
+		assertArrayEquals(ss.ConvertChar('.'), new int [][]
+			{{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0}});
+		
+		assertArrayEquals(ss.ConvertChar('\\'), new int [][]
+			{{1, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 1}});
+		
+		assertArrayEquals(ss.ConvertChar('|'), new int [][]
+			{{0, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0}});
+		
+		assertArrayEquals(ss.ConvertChar('-'), new int [][]
+			{{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{1, 1, 1, 1, 1},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0}});
+	}
+}
