bb33e55e89f46338b8af7d0d371065f9188d1280
unknown <ERontogi@nbwin1153.campus.tue.nl>
2015-10-07 16:56:40 +0200

    adding some "conceptual' functionality to the classes

--- src/eu/ooti/forumlite/MoviePlayer.java
+++ src/eu/ooti/forumlite/MoviePlayer.java
@@ -1,6 +1,23 @@
 package eu.ooti.forumlite;
 
+import java.util.List;
+
 public class MoviePlayer 
 {
+	MovieReader reader;
+	List MovieContents;
+	
+	public MoviePlayer()
+	{
+		reader = new MovieReader();
+	}
+	
+	
+	public void PlayMovie(String filename)
+	{
+		MovieContents = reader.ReadMovie(filename);
+		//more here soon
+	}
+	
 
 }
--- src/eu/ooti/forumlite/MovieReader.java
+++ src/eu/ooti/forumlite/MovieReader.java
@@ -1,6 +1,20 @@
 package eu.ooti.forumlite;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MovieReader 
 {
+	List movie;
+	
+	public MovieReader()
+	{
+		movie = new ArrayList();
+	}
+	public List ReadMovie(String filename)
+	{
+		//reading file here
+		return movie;
+	}
 
 }
