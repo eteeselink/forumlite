57685ad28ab344ce6d3751a03bdbbff2e4c2dcb2
unknown <ERontogi@nbwin1153.campus.tue.nl>
2015-10-08 09:17:06 +0200

    adding MovieReader test class

---
+++ test/eu/ooti/forumlite/MovieReaderTest.java
@@ -0,0 +1,19 @@
+package eu.ooti.forumlite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieReaderTest {
+
+	@Test
+	public void testMovieReader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadMovie() {
+		fail("Not yet implemented");
+	}
+
+}
