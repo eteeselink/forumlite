75890878670ffe462463637f6c07a07aa15d4e7d
fibice3 <kwratis@yahoo.gr>
2015-10-08 11:47:33 +0200

    display

--- src/eu/ooti/forumlite/Displayer.java
+++ src/eu/ooti/forumlite/Displayer.java
@@ -1,19 +1,22 @@
 package eu.ooti.forumlite;
 
-<<<<<<< HEAD
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.ScreenCharacterStyle;
 import com.googlecode.lanterna.terminal.Terminal.Color;
 
-
 public class Displayer implements AutoCloseable {
-	private final Screen screen;
+	private static Screen screen;
 	
+	//public LightSimulator() {
+	//	screen = TerminalFacade.createScreen();
+	//	screen.startScreen();
+	//}
+
 	public Displayer() {
 		screen = TerminalFacade.createScreen();
 		screen.startScreen();
-	}
+	}	
 	
 	public void close() throws Exception {
 		screen.stopScreen();
@@ -21,120 +24,78 @@ public class Displayer implements AutoCloseable {
 	
 	public Screen getScreen() {
 		return screen;
-=======
-public class Displayer {
+	}
+	
 
-
-	public static void display (int a[][])
-	{
-		for(int i=0;i<5;i++)
+	
+	
+	
+		public static void display (int a[][])
 		{
-			for(int j=0;j<5;j++)
-			{
-				if(a[i][j] == 1)		
-					System.out.print("*");
-				else
-					System.out.print(" ");
-			}
-			System.out.println("");
-		}
->>>>>>> 17df66b6dcf9f970386f2a231fe2563aa244d910
-	}
-	
+			
+			int[][] displayMatrix = new int[5][43];
 
-<<<<<<< HEAD
-	
-	
-	
-       /* public void displaySomething(Color foreground) throws InterruptedException {
-		screen.putString(20, 10, "    *   *   *     *   *   *    *   *   *     *   *   *", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-		screen.putString(20, 11, "      * * *         * * *        * * *         * * *  ", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-		screen.putString(20, 12, "    * * * * *     * * * * *    * * * * *     * * * * *", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-		screen.putString(20, 13, "      * * *         * * *        * * *         * * *  ", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-		screen.putString(20, 14, "    *   *   *     *   *   *    *   *   *     *   *   *", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-		screen.refresh();
-		Thread.sleep(2000);
-		
-		screen.clear();
-		screen.refresh();
-	}
-        */
-        
-        
-        
-        
-        public void displaySomething(Color foreground) throws InterruptedException {
-		
-            int i,j;
-            int a[][] = {
-					{1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0},
-                                        {1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
-                                        {1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
-                                        {1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
-                                        {1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1}
-					
-						};
-            
-            
-            
-            String ch;
-            for( i=0;i<5;i++)
+			
+			for(int i=0;i<5;i++)
 			{
-			for(j=0;j<25;j++)
+				for(int j=0;j<4;j++)
+				{
+					displayMatrix[i][j] = 0;
+				}
+				for(int j=4;j<9;j++)
+				{
+					displayMatrix[i][j] = a[i][j-4];
+				}
+				for(int j=9;j<14;j++)
+				{
+					displayMatrix[i][j] = 0;
+				}
+				for(int j=14;j<19;j++)
+				{
+					displayMatrix[i][j] = a[i][j-9];
+				}
+				for(int j=19;j<22;j++)
+				{
+					displayMatrix[i][j] = 0;
+				}
+				for(int j=22;j<27;j++)
+				{
+					displayMatrix[i][j] = a[i][j-13];
+				}
+				for(int j=27;j<38;j++)
+				{
+					displayMatrix[i][j] = 0;
+				}
+				for(int j=38;j<43;j++)
+				{
+					displayMatrix[i][j] = a[i][j-18];
+				}
+			}
+			
+			for(int i=0;i<5;i++)
 			{
-                            if(a[i][j] == 1)	
-						ch = "#";
+				for(int j=0;j<43;j++)
+				{
+					if(a[i][j] == 1)	
+						screen.putString(20, 10+i, "*", Color.RED, Color.BLACK, ScreenCharacterStyle.Blinking);
 					else
-						ch = " ";
-                            
-                            screen.putString(20+2*j,10+(i), ch, foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-                            
-                            
-                    
-                        
-                        
-                        }
-                        
-                        screen.putString(20+j,10+(i), " ", foreground, Color.BLACK, ScreenCharacterStyle.Blinking);
-                         
-            
-                       screen.refresh();
-                        Thread.sleep(2000);
-		
-                	//screen.clear();
-                        screen.refresh();
-                    
-             
-	}
-        }
-}
-=======
-	public static void displayString (int a[][])
-	{
-		for(int i=0;i<5;i++)
-		{
-			for(int j=0;j<25;j++)
-			{
-				if(a[i][j] == 1)		
-					System.out.print("*");
-				else
-					System.out.print(" ");
+						screen.putString(20, 10+i, " ", Color.RED, Color.BLACK, ScreenCharacterStyle.Blinking);
+				}
+				
 			}
-			System.out.println("");
-		}
-
+			
+			
+			screen.refresh();
+			try {
+				Thread.sleep(2000);
+				
+			} catch (InterruptedException e) {
+				
+			}
+		
+			
+			screen.clear();
+			screen.refresh();
 	}
 
-	public static void main(String args[])
-	{
-		int a[][] = {
-				{1,0,0,1,1},
-				{1,1,1,1,1},
-				{0,0,0,0,0},
-				{1,1,1,1,1},
-				{1,1,1,1,1},
-		};
-		display(a);
-	}
 }
->>>>>>> 17df66b6dcf9f970386f2a231fe2563aa244d910
